32716253b5460c8f1cd6c3ef99005d1ff58f55f5
Avinash Yadav Lungase <avinashlu@cybage.com>
2022-07-11 09:42:01 +0530

    first commit

---
+++ src/main/java/com/example/MokitoJunittesting/MokitoJunittestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.MokitoJunittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokitoJunittestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokitoJunittestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MokitoJunittesting/MokitoJunittestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MokitoJunittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokitoJunittestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
